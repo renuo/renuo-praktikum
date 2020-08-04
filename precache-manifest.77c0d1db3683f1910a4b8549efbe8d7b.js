156b4371f0df1fdb08ef05e1dac8bcd942ee8251
pandermatt <p.andermatt@me.com>
2020-08-04 11:33:53 +0000

    Deploying to gh-pages from  @ dac8936c709dc7ab4ac81799223c2bb7b8e23174 🚀

---
+++ precache-manifest.77c0d1db3683f1910a4b8549efbe8d7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2e3667aa5451f6c322c",
+    "url": "/css/app.017d2d70.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "97cee6f492f2f4d93881c4ea3316b2af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2e3667aa5451f6c322c",
+    "url": "/js/app.109f255d.js"
+  },
+  {
+    "revision": "20cb6c42939fea492130",
+    "url": "/js/chunk-vendors.e69b684f.js"
+  },
+  {
+    "revision": "729ee957fc26b1f72e474a17ed7d3840",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
