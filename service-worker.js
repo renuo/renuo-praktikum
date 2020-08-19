8c13c9d2e0727615c2d95d1dd8d9c821107ed6e6
Liberatys <Ilithium@protonmail.com>
2020-08-19 09:15:23 +0000

    Deploying to gh-pages from  @ 9309c758e905bce0ff1a75de65822b139dba489e 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ff5a5bbdd503d53afbba55be4d3b2db6.js"
+  "/precache-manifest.2cfcb9d736137f0555a0ab32f841b611.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "renuo-praktikum"});
