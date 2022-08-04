842ba4c33c1cb8aa3c01d8e3d06fd9c10c991162
Anes Hodza <anes.hodza@renuo.ch>
2022-08-04 19:06:23 +0200

    floating animation is done

--- src/components/Goodies.vue
+++ src/components/Goodies.vue
@@ -9,175 +9,223 @@
     <a href="https://renuo.ch/jobs" class="btn btn-info" role="button"
       >Komm zu uns ins Team</a
     >
-    <!--<svg viewBox="0 0 462 476" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg
+      viewBox="0 0 541 508"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      overflow="visible"
+    >
       <g id="Circles">
-        <ellipse id="investment" cx="183" cy="402.794" rx="75" ry="72.7941" fill="#A593CD"/>
-        <ellipse id="community" cx="60" cy="308.235" rx="60" ry="58.2353" fill="#A593CD"/>
-        <ellipse id="worklife" cx="80" cy="165.235" rx="60" ry="58.2353" fill="#A593CD"/>
-        <ellipse id="softdrinks" cx="328" cy="330.235" rx="60" ry="58.2353" fill="#A593CD"/>
-        <ellipse id="technology" cx="218" cy="98.2353" rx="60" ry="58.2353" fill="#A593CD"/>
-        <ellipse id="events" cx="338" cy="197.235" rx="60" ry="58.2353" fill="#A593CD"/>
-        <ellipse id="wallet" cx="149.212" cy="40" rx="41.2121" ry="40" fill="#A593CD"/>
-        <ellipse id="desk" cx="290.212" cy="47" rx="41.2121" ry="40" fill="#A593CD"/>
-        <ellipse id="week" cx="420.212" cy="147" rx="41.2121" ry="40" fill="#A593CD"/>
-        <ellipse id="coffee" cx="408.212" cy="370" rx="41.2121" ry="40" fill="#A593CD"/>
-        <ellipse id="goodies" cx="201" cy="246" rx="102" ry="99" fill="#29D28B"/>
-      </g>
-    </svg> -->
-    <svg viewBox="0 0 540 509" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <g id="Circles">
-        <circle id="investment" cx="202" cy="419" r="90" fill="#A593CD" />
-        <circle id="community" cx="74" cy="290" r="75" fill="#A593CD" />
-        <circle id="worklife" cx="131" cy="155" r="60" fill="#A593CD" />
-        <circle id="softdrinks" cx="371" cy="363" r="75" fill="#A593CD" />
-        <circle id="technology" cx="282" cy="120" r="75" fill="#A593CD" />
-        <circle id="wallet" cx="194" cy="53" r="45" fill="#A593CD" />
-        <circle id="events" cx="386" cy="227" r="60" fill="#A593CD" />
-        <circle id="week" cx="480" cy="171" r="60" fill="#A593CD" />
-        <circle id="coffee" cx="465" cy="419" r="45" fill="#A593CD" />
-        <circle id="desk" cx="386" cy="60" r="60" fill="#A593CD" />
-        <circle id="goodies" cx="242" cy="258" r="100" fill="#29D28B" />
-        <text
-          id="coffee_text"
-          fill="white"
-          xml:space="preserve"
-          style="white-space: pre"
-          font-family="Montserrat"
-          font-size="20"
-          font-weight="bold"
-          letter-spacing="0em"
-        >
-          <tspan x="435.531" y="414.17">Gratis&#x2028;</tspan>
-          <tspan x="432.182" y="438.17">Kaffee</tspan>
-        </text>
-        <text
-          id="week_text"
-          fill="white"
-          xml:space="preserve"
-          style="white-space: pre"
-          font-family="Montserrat"
-          font-size="20"
-          font-weight="bold"
-          letter-spacing="0em"
-        >
-          <tspan x="440.627" y="154.17">Code &#38; &#x2028;</tspan>
-          <tspan x="434.006" y="178.17">Learning&#x2028;</tspan>
-          <tspan x="450.197" y="202.17">Week</tspan>
-        </text>
-        <text
-          id="technology_text"
-          fill="white"
-          xml:space="preserve"
-          style="white-space: pre"
-          font-family="Montserrat"
-          font-size="20"
-          font-weight="bold"
-          letter-spacing="0em"
-        >
-          <tspan x="229.668" y="111.17">Neuste IT-&#x2028;</tspan>
-          <tspan x="218.379" y="135.17">Technologie</tspan>
-        </text>
-        <text
-          id="wallet_text"
-          fill="white"
-          xml:space="preserve"
-          style="white-space: pre"
-          font-family="Montserrat"
-          font-size="20"
-          font-weight="bold"
-          letter-spacing="0em"
-        >
-          <tspan x="157.174" y="48.17">Renuo &#10;</tspan>
-          <tspan x="160.074" y="72.17">Wallet</tspan>
-        </text>
-        <text
-          id="goodies_text"
-          fill="white"
-          xml:space="preserve"
-          style="white-space: pre"
-          font-family="Montserrat"
-          font-size="36"
-          font-weight="bold"
-          letter-spacing="0em"
-        >
-          <tspan x="166.229" y="267.906">Goodies</tspan>
-        </text>
-        <text
-          id="desk_text"
-          fill="white"
-          xml:space="preserve"
-          style="white-space: pre"
-          font-family="Montserrat"
-          font-size="20"
-          font-weight="bold"
-          letter-spacing="0em"
-        >
-          <tspan x="335.055" y="67.17">Stehpulte</tspan>
-        </text>
-        <text
-          id="events_text"
-          fill="white"
-          xml:space="preserve"
-          style="white-space: pre"
-          font-family="Montserrat"
-          font-size="20"
-          font-weight="bold"
-          letter-spacing="0em"
-        >
-          <tspan x="354.186" y="222.17">Team-&#x2028;</tspan>
-          <tspan x="351.129" y="246.17">Events</tspan>
-        </text>
-        <text
-          id="softdrinks_text"
-          fill="white"
-          xml:space="preserve"
-          style="white-space: pre"
-          font-family="Montserrat"
-          font-size="20"
-          font-weight="bold"
-          letter-spacing="0em"
-        >
-          <tspan x="314.33" y="358.17">Softdrinks &#10;</tspan>
-          <tspan x="339.623" y="382.17">&#38; Bier</tspan>
-        </text>
-        <text
-          id="community_text"
-          fill="white"
-          xml:space="preserve"
-          style="white-space: pre"
-          font-family="Montserrat"
-          font-size="20"
-          font-weight="bold"
-          letter-spacing="0em"
-        >
-          <tspan x="12.3105" y="297.17">Community</tspan>
-        </text>
-        <text
-          id="worklife_text"
-          fill="white"
-          xml:space="preserve"
-          style="white-space: pre"
-          font-family="Montserrat"
-          font-size="20"
-          font-weight="bold"
-          letter-spacing="0em"
-        >
-          <tspan x="80.2402" y="152.17">Work-Life&#x2028;</tspan>
-          <tspan x="89.4004" y="176.17">Balance</tspan>
-        </text>
-        <text
-          id="investment_text"
-          fill="white"
-          xml:space="preserve"
-          style="white-space: pre"
-          font-family="Montserrat"
-          font-size="20"
-          font-weight="bold"
-          letter-spacing="0em"
-        >
-          <tspan x="132.402" y="409.17">Investment &#38;&#x2028;</tspan>
-          <tspan x="126.436" y="433.17">Selbststudium</tspan>
-        </text>
+        <g id="coffee">
+          <circle id="coffee_circle" cx="466" cy="418" r="45" fill="#A593CD" />
+          <text
+            id="coffee_text"
+            fill="white"
+            xml:space="preserve"
+            style="white-space: pre"
+            font-family="Montserrat"
+            font-size="20"
+            font-weight="bold"
+            letter-spacing="0em"
+          >
+            <tspan x="436.531" y="413.17">Gratis&#x2028;</tspan>
+            <tspan x="433.182" y="437.17">Kaffee</tspan>
+          </text>
+        </g>
+        <g id="week">
+          <circle id="week_circle" cx="481" cy="170" r="60" fill="#A593CD" />
+          <text
+            id="week_text"
+            fill="white"
+            xml:space="preserve"
+            style="white-space: pre"
+            font-family="Montserrat"
+            font-size="20"
+            font-weight="bold"
+            letter-spacing="0em"
+          >
+            <tspan x="441.627" y="153.17">Code &#38; &#x2028;</tspan>
+            <tspan x="435.006" y="177.17">Learning&#x2028;</tspan>
+            <tspan x="451.197" y="201.17">Week</tspan>
+          </text>
+        </g>
+        <g id="technology">
+          <circle
+            id="technology_circle"
+            cx="283"
+            cy="119"
+            r="75"
+            fill="#A593CD"
+          />
+          <text
+            id="technology_text"
+            fill="white"
+            xml:space="preserve"
+            style="white-space: pre"
+            font-family="Montserrat"
+            font-size="20"
+            font-weight="bold"
+            letter-spacing="0em"
+          >
+            <tspan x="230.668" y="110.17">Neuste IT-&#x2028;</tspan>
+            <tspan x="219.379" y="134.17">Technologie</tspan>
+          </text>
+        </g>
+        <g id="wallet">
+          <circle id="wallet_circle" cx="195" cy="52" r="45" fill="#A593CD" />
+          <text
+            id="wallet_text"
+            fill="white"
+            xml:space="preserve"
+            style="white-space: pre"
+            font-family="Montserrat"
+            font-size="20"
+            font-weight="bold"
+            letter-spacing="0em"
+          >
+            <tspan x="158.174" y="47.17">Renuo &#10;</tspan>
+            <tspan x="161.074" y="71.17">Wallet</tspan>
+          </text>
+        </g>
+        <g id="desk">
+          <circle id="desk_circle" cx="387" cy="60" r="60" fill="#A593CD" />
+          <text
+            id="desk_text"
+            fill="white"
+            xml:space="preserve"
+            style="white-space: pre"
+            font-family="Montserrat"
+            font-size="20"
+            font-weight="bold"
+            letter-spacing="0em"
+          >
+            <tspan x="336.055" y="66.17">Stehpulte</tspan>
+          </text>
+        </g>
+        <g id="events">
+          <circle id="events_circle" cx="387" cy="226" r="60" fill="#A593CD" />
+          <text
+            id="events_text"
+            fill="white"
+            xml:space="preserve"
+            style="white-space: pre"
+            font-family="Montserrat"
+            font-size="20"
+            font-weight="bold"
+            letter-spacing="0em"
+          >
+            <tspan x="355.186" y="221.17">Team-&#x2028;</tspan>
+            <tspan x="352.129" y="245.17">Events</tspan>
+          </text>
+        </g>
+        <g id="softdrinks">
+          <circle
+            id="softdrinks_circle"
+            cx="372"
+            cy="362"
+            r="75"
+            fill="#A593CD"
+          />
+          <text
+            id="softdrinks_text"
+            fill="white"
+            xml:space="preserve"
+            style="white-space: pre"
+            font-family="Montserrat"
+            font-size="20"
+            font-weight="bold"
+            letter-spacing="0em"
+          >
+            <tspan x="315.33" y="357.17">Softdrinks &#10;</tspan>
+            <tspan x="340.623" y="381.17">&#38; Bier</tspan>
+          </text>
+        </g>
+        <g id="community">
+          <circle
+            id="community_circle"
+            cx="75"
+            cy="289"
+            r="75"
+            fill="#A593CD"
+          />
+          <text
+            id="community_text"
+            fill="white"
+            xml:space="preserve"
+            style="white-space: pre"
+            font-family="Montserrat"
+            font-size="20"
+            font-weight="bold"
+            letter-spacing="0em"
+          >
+            <tspan x="13.3105" y="296.17">Community</tspan>
+          </text>
+        </g>
+        <g id="investment">
+          <circle
+            id="investment_circle"
+            cx="203"
+            cy="418"
+            r="90"
+            fill="#A593CD"
+          />
+          <text
+            id="investment_text"
+            fill="white"
+            xml:space="preserve"
+            style="white-space: pre"
+            font-family="Montserrat"
+            font-size="20"
+            font-weight="bold"
+            letter-spacing="0em"
+          >
+            <tspan x="133.402" y="408.17">Investment &#38;&#x2028;</tspan>
+            <tspan x="127.436" y="432.17">Selbststudium</tspan>
+          </text>
+        </g>
+        <g id="worklife">
+          <circle
+            id="worklife_circle"
+            cx="132"
+            cy="154"
+            r="60"
+            fill="#A593CD"
+          />
+          <text
+            id="worklife_text"
+            fill="white"
+            xml:space="preserve"
+            style="white-space: pre"
+            font-family="Montserrat"
+            font-size="20"
+            font-weight="bold"
+            letter-spacing="0em"
+          >
+            <tspan x="81.2402" y="151.17">Work-Life&#x2028;</tspan>
+            <tspan x="90.4004" y="175.17">Balance</tspan>
+          </text>
+        </g>
+        <g id="goodies">
+          <circle
+            id="goodies_circle"
+            cx="243"
+            cy="257"
+            r="100"
+            fill="#29D28B"
+          />
+          <text
+            id="goodies_text"
+            fill="white"
+            xml:space="preserve"
+            style="white-space: pre"
+            font-family="Montserrat"
+            font-size="36"
+            font-weight="bold"
+            letter-spacing="0em"
+          >
+            <tspan x="167.229" y="266.906">Goodies</tspan>
+          </text>
+        </g>
       </g>
     </svg>
   </div>
@@ -193,7 +241,55 @@ export default {
 
 <style scoped lang="scss">
 @use "../stylesheets/global.scss";
-g#Circles :nth-child(1) {
-  z-index: 1;
+g#Circles {
+  overflow: visible;
+}
+
+@keyframes float {
+  0% {
+    transform: translateY(2px);
+  }
+  50% {
+    transform: translateY(-2px);
+  }
+}
+
+g {
+  animation: float 3s infinite ease-in-out;
+  transform: translateY(2px);
+}
+
+g#worklife {
+  animation-delay: 200ms;
+}
+g#coffee {
+  animation-delay: 150ms;
+}
+g#investment {
+  animation-delay: 400ms;
+}
+g#community {
+  animation-delay: 600ms;
+}
+g#softdrinks {
+  animation-delay: 50ms;
+}
+g#events {
+  animation-delay: 0ms;
+}
+g#desk {
+  animation-delay: 300ms;
+}
+g#goodies {
+  animation-delay: 250ms;
+}
+g#wallet {
+  animation-delay: 150ms;
+}
+g#technology {
+  animation-delay: 400ms;
+}
+g#week {
+  animation-delay: 200ms;
 }
 </style>
