eb84dc5a5aee01eb119e038c10c23f5aed430c62
Colin <colin.zimmermann@renuo.ch>
2022-08-08 16:29:17 +0200

    add link and button

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <h2 id="about-renuo">Das Team</h2>
+    <a href="https://renuo.ch/team" class="btn btn-info" role="button"
+      >Komm zu uns ins Team</a
+    >
     <img class="img-fluid" alt="renuo team" src="@/assets/team.jpg" />
+    >
   </div>
 </template>
 <script>
