ff883479a4e7d63d7b54f0d5bf05e0fdb4b8c877
Pascal Andermatt <p.andermatt@me.com>
2022-08-10 14:38:27 +0200

    fix lint

--- src/components/Notice.vue
+++ src/components/Notice.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="px-5 alert alert-success">
-    Um die Bäume 🌳 zu retten, haben wir beschlossen, keine Flyer mehr zu drucken.
-    So kannst du dich an dieser Website erfreuen, die mithilfe unserer Praktikanten erstellt wurde.
-    Für Praktikanten von Praktikanten.
+    Um die Bäume 🌳 zu retten, haben wir beschlossen, keine Flyer mehr zu
+    drucken. So kannst du dich an dieser Website erfreuen, die mithilfe unserer
+    Praktikanten erstellt wurde. Für Praktikanten von Praktikanten.
   </div>
 </template>
 
 <script>
 export default {
   name: "Notice"
-}
+};
 </script>
 
 <style scoped>
@@ -17,5 +17,4 @@ div {
   border-radius: 0;
   margin-bottom: 0;
 }
-
 </style>
