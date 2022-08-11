29cdf3b29a47d96e5a6f41bf567d454374b353df
Anes Hodza <anes.hodza@renuo.ch>
2022-08-11 11:17:12 +0200

    finish instagram component

---
+++ src/components/Instagram.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="instagram-grid" ref="instagram-grid">
+    <img
+      v-for="index in 25"
+      :key="index"
+      :src="require(`@/assets/instagram/image-${index}.jpeg`)"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Instagram.vue"
+};
+</script>
+
+<style scoped lang="scss">
+div.instagram-grid {
+  width: 90%;
+  margin: 0 auto;
+  aspect-ratio: 1;
+  display: grid;
+  grid-column-gap: 2%;
+  grid-row-gap: 2%;
+  grid-template-rows: repeat(5, 18.4%);
+  grid-template-columns: repeat(5, 18.4%);
+  img {
+    width: 100%;
+    aspect-ratio: 1;
+    opacity: 40%;
+    transition: all 0.75s ease-in-out;
+    &:hover {
+      opacity: 100%;
+      transition: all 50ms ease-in-out;
+    }
+  }
+}
+</style>
